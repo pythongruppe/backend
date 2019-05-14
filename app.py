e03ea7b8f1754541a77d366b8153f7bb5936e1e8
mkl <mkl@mikiaholding.dk>
2019-05-14 15:52:04 +0200

    changed to .csv backend and added functionality to add data to a csv file if it does not exist

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
-from flask import Flask
+from flask import Flask, request
+from Memory import input_to_database
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == 'POST':
+        #assumed request.data is json with proper formatting / attributes NO checking atm.
+        return input_to_database(request.data)
     return 'Hello World!'
+
+
+#@app.route('/getAllIds', methods=['GET'])
+#def show_ids():
+    #return house_ids()
 
 
 if __name__ == '__main__':
     app.run()
+
+
+#check i can get data
+#check i can read to file
+#check i will not read to file if already exists
+#create unit tests for this
+#refactor methods in memory.py into smaller methods
