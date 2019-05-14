2550a746fb089a2832ad9287d4cacf921015c6af
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-05-14 14:32:36 +0200

    added argumentparser for bolighed webscraper

--- src/webscrapers/bolighed_webscraper.py
+++ src/webscrapers/bolighed_webscraper.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import pandas as pd
-import numpy as np
+import argparse
 
 columns_to_drop = [
     "andelsbolig",
@@ -34,7 +34,6 @@ columns_to_rename = {
     "udbetaling": "down_payment"
 }
 
-
 def find_results_page(page_number, page_size):
     offset = (page_number - 1) * page_size
     url = f'https://bolighed.dk/api/external/market/propertyforsale/?limit={page_size}&offset={offset}&view=list&ordering=mtid'
@@ -43,7 +42,7 @@ def find_results_page(page_number, page_size):
     return json.loads(response.content)['results']
 
 
-def find_results():
+def find_results(max_results):
     results = []
     page_number = 1
     page_size = 1000
@@ -52,15 +51,21 @@ def find_results():
         page_results = find_results_page(page_number, page_size)
         results.extend(page_results)
         page_number += 1
-        if len(page_results) < page_size:
+        if len(page_results) < page_size or len(results) >= max_results:
+            results = results[0:max_results]
             frame = pd.DataFrame(results)
             frame.drop(columns_to_drop, axis=1, inplace=True)
             frame.rename(columns=columns_to_rename, inplace=True)
             return frame
 
 
-save_name = "properties.csv"
 if __name__ == '__main__':
-    frame = find_results()
-    frame.to_csv(save_name, index=False)
-    print(f'Saved to {save_name}')
+    parser = argparse.ArgumentParser(description='Webscrape data from bolighed.dk')
+    parser.add_argument('output', help='Path to output file')
+    parser.add_argument('-m', '--max_results', type=int, default=10_000_000, help='Maximum number of results')
+
+    args = vars(parser.parse_args())
+
+    frame = find_results(args["max_results"])
+    frame.to_csv(args["output"], index=False)
+    print(f'Saved to {args["output"]}')
