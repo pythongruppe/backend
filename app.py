f6aad45058aa5964a17c6f213538b005a4feff32
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-06-10 11:22:14 +0200

    clean

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 from src.collection.types import PropertyType
 from src.logic.graphs import create_distribution_graphs, create_graph_cache
 from src.logic.prediction import create_cost_predictor
-from src.machine_learning.estate_type_prediction import create_estimator
+from src.machine_learning.estate_type_prediction import create_pt_classifier
 import pandas as pd
 
 app = Flask(__name__, static_url_path='/static', static_folder='public')
@@ -17,8 +17,7 @@ CORS(app)
 memory = None
 graph_cache = None
 cost_predictor = None
-property_predictor = None
-
+prop_type_predictor, ptc_acc = None, None
 
 
 def paginate(df, page_size, page_number):
@@ -31,19 +30,18 @@ def paginate(df, page_size, page_number):
 def graphs():
     return jsonify(create_distribution_graphs(graph_cache))
 
-#
-# @app.route('/static/<path:path>')
-# def serve_static(path):
-#     return send_from_directory('public', path)
 
-@app.route('/estate', methods=["POST"])
-def classifer_prediction():
+@app.route('/predict-property-type', methods=["POST"])
+def classifier_prediction():
     content = request.get_json()
-    d = dict(content)
-    df = pd.DataFrame.from_dict({key: [value] for key, value in d.items()})
-    return jsonify(str(property_predictor(df)[0]))
-
-
+    user_data = dict(content)
+    user_data_df = pd.DataFrame.from_dict({key: [value] for key, value in user_data.items()})
+    prediction = int(prop_type_predictor(user_data_df)[0])
+    return jsonify({
+        'name': PropertyType(prediction).name,
+        'value': prediction,
+        'accuracy': ptc_acc
+    })
 
 
 @app.route('/filter', methods=['POST'])
@@ -78,12 +76,14 @@ def cost_prediction():
     sent = dict(request.json)
     return jsonify(cost_predictor(sent))
 
+
 if __name__ == '__main__':
     print('Loading memory ...')
     memory = Memory('new_data.csv')
     graph_cache = create_graph_cache(memory.data)
     print(f'Loaded {len(memory.data)} records.')
     cost_predictor = create_cost_predictor(memory.data)
-    property_predictor = create_estimator(memory.data)
+    prop_type_predictor, ptc_acc = create_pt_classifier(memory.data)
+    print(f'Trained property type classifier, accuracy={ptc_acc}')
 
     app.run()
--- src/machine_learning/estate_type_prediction.py
+++ src/machine_learning/estate_type_prediction.py
@@ -1,34 +1,24 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-import numpy as np
-import pandas as pd
 
-data = pd.read_csv("new_data.csv")
 columns_keep = ["area_basement", "area_estate", "area_property", "rooms", "year", "zip"]
 
-def create_estimator(data):
+
+def create_pt_classifier(data):
     knn = KNeighborsClassifier()
     df = data
     df = df.fillna(0)
     y = df["property_type"]
     X = df[columns_keep]
     knn = KNeighborsClassifier(n_neighbors=3)
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-    knn.fit(X,y)
-    #pred = knn.predict(X_test)
-    #print(accuracy_score(y_test, pred))
+    knn.fit(X, y)
+    acc = accuracy_score(y_test, knn.predict(X_test))
 
-    def f(userdata):
-        x = knn.predict(userdata)
+    def f(user_data):
+        x = knn.predict(user_data)
         return x
-    return f
 
-#property_predictor = _create_estimator(data)
-
-
-def _decide_best_neighbors():
-    return None
-#data.fillna(data.mean(), inplace=True)
+    return f, acc
