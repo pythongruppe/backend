f3e2e29bec2a062371d366c5bb0937dd7f52971c
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2019-06-15 11:15:06 +0200

    added unit and max arguments to choropleth generation

--- src/processing/choropleth.py
+++ src/processing/choropleth.py
@@ -13,7 +13,7 @@ def download_zip_bounds():
         f.write(response.text)
 
 
-def create_choropleth_map(df, zip_bounds, key, save_file, query):
+def create_choropleth_map(df, zip_bounds, key, save_file, query, max, unit):
     df = df.dropna(subset=[key, 'zip'])
     if query is not None:
         df = df.query(query)
@@ -30,8 +30,10 @@ def create_choropleth_map(df, zip_bounds, key, save_file, query):
     means = df[['zip', key]].groupby('zip').mean()
     means['zip'] = means.index
 
-    means = means[means[key] < 10_000_000]
-    bins = np.geomspace(means[key].min() - 1, means[key].max() + 1, num=9)
+    if max is not None:
+        means = means[means[key] < max]
+    means[key] = means[key] / unit
+    bins = np.geomspace(means[key].min() - (1 / unit), means[key].max() + (1 / unit), num=9)
 
     c_map = folium.Choropleth(
         geo_data=zip_bounds,
@@ -68,6 +70,9 @@ if __name__ == '__main__':
     parser.add_argument('key',
                         help="The key in the dataset, that is plotted on the heat-map. Use multiple keys by seperating the keys with commas.")
     parser.add_argument('-f', '--filter', type=str, default=None, help='An optional pandas query to apply to the data.')
+    parser.add_argument('-m', '--max', type=int, default=None, help='The upper bound for the data points.')
+    parser.add_argument('-u', '--unit', type=int, default=1,
+                        help='When given, all data points are first divided by the provided factor.')
 
     args = vars(parser.parse_args())
     data = pd.read_csv(args['input'])
@@ -77,6 +82,6 @@ if __name__ == '__main__':
         features = json.loads(f.read())
         for key in keys:
             output = args['output'].replace('*', key)
-            _, _, data = create_choropleth_map(data, features, key, output, args['filter'])
+            _, _, data = create_choropleth_map(data, features, key, output, args['filter'], args['max'], args['unit'])
             print(f'{len(data)} properties were used to create the choropleth with key {key}.')
             print(f'Saved to {output}')
